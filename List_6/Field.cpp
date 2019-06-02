d1188af16cd9e9d8a267dfb9b3b5187534c9734d
piotr.kawa <piotr.w.kawa@gmail.com>
2019-06-02 08:40:28 +0200

    Elliptic Curves

---
+++ List_6/Field.cpp
@@ -0,0 +1,97 @@
+/*--------------------- field.cpp ---------------- */
+
+#include "gmpxx.h"
+
+// mpz_t - typ z c sluzacy do przechowywania duzych liczb
+
+template <mpz_t mod>
+class Field
+{
+  private:
+    mpz_class value;
+
+  public:
+    // Field() {}
+
+    Field(mpz_t value)
+    {
+        mpz_mod(this->value.get_mpz_t(), value, mod);
+    }
+
+    Field(mpz_class value)
+    {
+        // gdzie
+        // co
+        // modulo
+        mpz_mod(this->value.get_mpz_t(), value.get_mpz_t(), mod);
+    }
+
+    Field(int value)
+    {
+        mpz_t tmp;
+        // set signed integer
+        mpz_init_set_si(tmp, value);
+        mpz_mod(this->value.get_mpz_t(), tmp, mod);
+    }
+
+    // Field(long long value)
+    // {
+    //     mpz_t tmp;
+    //     mpz_init_set_si(tmp, value);
+    //     mpz_mod(this->value.get_mpz_t(), tmp, mod);
+    // }
+
+    void getValue(mpz_t ret)
+    {
+        mpz_init_set(ret, this->value.get_mpz_t());
+    }
+
+    mpz_class getValue()
+    {
+        return this->value;
+    }
+
+    Field operator+(Field const &obj)
+    {
+        return Field(value + obj.value);
+    }
+
+    Field operator-(Field const &obj)
+    {
+        return Field(value - obj.value);
+    }
+
+    Field operator-()
+    {
+        return Field(-value);
+    }
+
+    Field operator*(Field const &obj)
+    {
+        return Field(value * obj.value);
+    }
+
+    Field operator/(Field const &obj)
+    {
+        mpz_t tmp;
+        mpz_init(tmp);
+        mpz_invert(tmp, obj.value.get_mpz_t(), mod);
+        mpz_mul(tmp, value.get_mpz_t(), tmp);
+        return Field(tmp);
+    }
+
+    Field operator%(Field const &obj)
+    {
+        return Field(value % obj.value);
+    }
+
+    bool operator==(Field const &rhs)
+    {
+        return mpz_cmp(value.get_mpz_t(), rhs.value.get_mpz_t()) == 0;
+    }
+
+    bool operator!=(Field const &rhs)
+    {
+        return mpz_cmp(value.get_mpz_t(), rhs.value.get_mpz_t()) != 0;
+    }
+};
